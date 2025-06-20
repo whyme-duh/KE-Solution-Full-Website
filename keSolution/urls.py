8c880fbdf71eae2b5f2f82cb33e85d8f192fecbe
whyme <ritikshrestha94@gmail.com>
2025-06-20 13:03:20 +0545

    little UI change

--- keSolution/urls.py
+++ keSolution/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('users/', include('Users.urls')),
+    path('accounts/', include('Users.urls')),
     path("login/", auth_views.LoginView.as_view(template_name = 'Users/login.html', redirect_authenticated_user = True), name = "login"),
     path("logout/", auth_views.LogoutView.as_view(template_name = 'Users/logout.html'), name = "logout")
 ]
