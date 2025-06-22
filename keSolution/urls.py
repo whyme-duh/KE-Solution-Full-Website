910a87d82054a067475fcd6bc457289db6ce6740
whyme <ritikshrestha94@gmail.com>
2025-06-22 20:08:00 +0545

    few changes

--- keSolution/urls.py
+++ keSolution/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('Users.urls')),
-    path("login/", auth_views.LoginView.as_view(template_name = 'Users/login.html', redirect_authenticated_user = True), name = "login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name = 'Users/logout.html'), name = "logout")
+    path("login/", auth_views.LoginView.as_view(template_name = 'users/login.html', redirect_authenticated_user = True), name = "login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = "logout")
 ]
