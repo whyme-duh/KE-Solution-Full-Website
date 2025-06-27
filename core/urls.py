443db543ec6679bb2e85ca27d9c9dfe778f4563b
whyme-duh <ritikshrestha94@gmail.com>
2025-06-27 19:40:57 +0545

    few adding in UI

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . views import index, llc_view, invoice_bookkeeping_view
+from . views import index, llc_view, invoice_bookkeeping_view, services_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name ='index'),
+    path('services/', services_view, name ='services'),
     path('llc/', llc_view, name ='llc_view'),
     path('bookkeeping/',invoice_bookkeeping_view, name ='invoice_bookkeeping')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -6,6 +6,8 @@ def index(request):
     services = Service.objects.all()
     return render (request, 'core/home.html', {"services": services})
 
+def services_view(request):
+    return render (request, 'core/services/servicelist.html')
 
 def llc_view(request):
     return render (request, 'core/services/llc.html')
